19d628c5b997130672fb8aa7cdce0ef5e2266702
spallya-omar <spallya@outlook.com>
2017-06-11 03:19:53 +0530

    Added configurations

---
+++ config/index.js
@@ -0,0 +1,17 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({silent: true});
+}
+
+const processType = process.env.PROCESS_TYPE;
+
+let config;
+try {
+  config = require(`./${processType}`)
+} catch (e) {
+  if (e.code === 'MODULE_NOT_FOUND') {
+    throw new Error(`No config found for process type: ${processType}`);
+  }
+  throw e;
+}
+
+module.exports = config;
